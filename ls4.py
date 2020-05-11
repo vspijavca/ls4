3b38619cbc5ab2468aedaf644ae1ed01fe2ff04d
Spijavca Veaceslav <dreamersf@gmail.com>
2020-05-12 00:05:52 +0300

    Module vspij

--- ls4.py
+++ ls4.py
@@ -2,3 +2,8 @@ from my_package import mult3
 
 print("Сумма введенных трех цифр")
 print(mult3(4, 10, 6))
+
+from my_package import длинна
+
+print("Длинна всех символов")
+print(длинна("erty", "morty"))
